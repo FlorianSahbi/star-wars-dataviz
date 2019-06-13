9937474c510132a418e036636e25f90d406a6ef6
Quentin <quentin.lenglin@gmail.com>
2019-06-13 17:38:25 +0200

    Added intro

--- front/src/Intro.js
+++ front/src/Intro.js
@@ -1,44 +1,58 @@
-import React, {useRef, useEffect} from 'react';
+import React, { Component } from "react";
+import { TweenLite } from "gsap";
+import { Power2, TimelineLite } from "gsap";
 
 import './Intro.css';
 
+import logo from './assets/logo.svg';
 
-function Intro() {
-  let byline = "A CONNECTED GALAXY"
-let bylineArry = byline.split('');
-let span;
-let letter;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.intro = React.createRef();
+    this.logo = React.createRef();
+    this.content = React.createRef();
+}
 
-  const bylineDomEl = useRef();
-  console.log(bylineDomEl);
-useEffect(()=>{
-  for (let i = 0; i < bylineArry.length; i++) {
-      const element = bylineArry[i];
-      span = document.createElement("span");
-      letter = document.createTextNode(element);
-      
-      if (element == '') {
-        bylineDomEl.current.appendChild(letter);
-        
-      }else{
-        span.appendChild(letter);
-        bylineDomEl.current.appendChild(span);
-      
-      }
+  componentDidMount() {
+    const tl = new TimelineLite();
     
+    tl
+      .to(this.intro.current, 4.5, { opacity: 1, delay: 1 })
+      .to(this.intro.current, 1.5, { opacity: 0 })
+      .set(this.logo.current, { opacity: 1, scale: 2.75 })
+      .to(this.logo.current, 8, { 
+        scale: 0.05, 
+        ease: Power2.easeOut
+      })
+      .to(this.logo.current, 1.5, { opacity: 0 }, "-=1.5")
+      .to(this.content.current, 200, { top: "-170%" });
+      
   }
-}, [])
 
-return (
-    <div className="App">
-      <div className = "starwars-demo" >
-        <img src="//cssanimation.rocks/demo/starwars/images/star.svg" alt="Star" className="star"></img>
-        <img src = "//cssanimation.rocks/demo/starwars/images/wars.svg" alt="Wars" className="wars"></img>
-        <h2 ref={bylineDomEl} className="byline" id="byline"></h2>
-      </div>
+render() {
+  return (
+    <div className="container">
+      <section className="intro" ref={this.intro}>
+        <p>
+          A long time ago, in a galaxy far,<br /> far away....
+        </p>
+      </section>
+      <section className="logo" ref={this.logo}>
+        <img src={logo} alt="Code Wars logo" />
+      </section>
+      <section className="crawl">
+        <div className="content" ref={this.content}>
+          <h1 className="title">Episode 7</h1>
+          <h2 className="subtitle">AN INTERCONNECTED GALAXY</h2>
+          <p>The Star Wars Galaxy is a vast system. Multiple characters of different species are spread across dozens of different planets.</p>
+          <p>Hoping to resolve the matter with a blockade of deadly battleships, the greedy Trade Federation has stopped all shipping to the small planet of Naboo.</p>
+          <p>And through seven movies, the Star Wars saga showed us a lot of intricate relationship between characters...</p>
+        </div>
+      </section>
     </div>
   );
 }
+}
 
-export default Intro;
-
+export default App;
--- front/src/Router.js
+++ front/src/Router.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Graph from './Graph';
-import Title from './Title';
+import Intro from './Intro';
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function ReactRouter() {
     return (
       <Router>
           <Route exact path="/" component={Graph} />
-          <Route exact path="/intro" component={Title} />
+          <Route exact path="/intro" component={Intro} />
       </Router>
     );
   }
